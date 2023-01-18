764d9cd75ade41138a4e911d19aba9b8dfbef451
Quinn <jacobqbastian@gmail.com>
2023-01-18 11:21:36 -0700

    W03 Work

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,28 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._company = "Idaho Dunes RV";
+        job1._jobTitle= "Manager";
+        job1._startYear = 2016;
+        job1._endYear = 2022;
+
+        Job job2 = new Job();
+        job2._company = "BYUI";
+        job2._jobTitle= "Surplus";
+        job2._startYear = 2020;
+        job2._endYear = 2022;
+
+        //job1.Display();
+        //job2.Display();
+        
+        Resume myResume = new Resume();
+        myResume._name = "Quinn Bastian";
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
+
+
     }
 }
