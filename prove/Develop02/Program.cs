9603e3c7861b8f01cbe9b7afdb35c26072b73b13
Quinn <jacobqbastian@gmail.com>
2023-01-22 17:55:07 -0700

    W03 Work

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -4,36 +4,41 @@ class Program
 {
     static void Main(string[] args)
     {
+       string userInput = null; 
        Console.WriteLine("Welcome to the Journal Program!");
        Console.WriteLine("Please select one of the following choices:");
-       string input = Console.ReadLine();
-       if (input == "1")
+       Console.WriteLine("1. Write");
+       Console.WriteLine("2. Display");
+       Console.WriteLine("3. Load");
+       Console.WriteLine("4. Save");
+       Console.WriteLine("5. Quit");
+       Console.Write("What would you like to do? ");
+       userInput = Console.ReadLine();
+
+       if (userInput == "1")
+        {
+        Console.WriteLine("Test 1");
+        }
+
+       else if (userInput == "2")
        {}
 
-       else if (input == "2")
+       else if (userInput == "3")
        {}
 
-       else if (input == "3")
+       else if (userInput == "4")
        {}
 
-
-       else if (input == "4")
-       {}
-
-       else if (input == "5")
+       else if (userInput == "5")
        {}
 
        else
-       {}
+       {
+            Console.WriteLine("Invalid input.");
+       }
 
     }
 }
-
-
-
-
-
-
 
 // class for entries and a class for prompts
 // display method for each class
